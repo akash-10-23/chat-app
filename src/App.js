f7c37fb9faa24696c3d73e2c8ec5cdf9a4ba9fdc
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-09-03 23:25:58 +0530

    Chat Feed layout set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import "./App.css";
+
+require('dotenv').config();
+
+const projectId = process.env.PROJECTID;
+const username = process.env.USERNAME;
+const usersceret = process.env.USER_SECRET;
+
+const App = () =>{
+
+    return (
+
+        <ChatEngine
+			height="100vh"
+			userName="Infinium"
+			userSecret="ThisIsNaruto"
+			projectID="d5b60537-b785-4e16-bf7d-4d366a5ad8a5"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		/>
+
+        // <ChatEngine 
+        //     height="100vh"
+        //     userName={username}
+		// 	userSecret={usersceret}
+		// 	projectID={projectId}
+        // />
+    );
+};
 
 export default App;
---
+++ src/components/UserMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserMessage(){
+    return (
+        <div>
+            UserMessage
+        </div>
+    );
+}
+
+export default UserMessage;
