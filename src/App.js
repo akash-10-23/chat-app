bfd69be2b8ecad856d307734a54ba141edc45255
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-09-04 18:01:47 +0530

    Add login page and deploy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import Login from "./components/Login";
 import "./App.css";
 
 require('dotenv').config();
 
-const projectId = process.env.PROJECTID;
-const username = process.env.USERNAME;
-const usersceret = process.env.USER_SECRET;
+const projectId ="d5b60537-b785-4e16-bf7d-4d366a5ad8a5";
 
 const App = () =>{
 
-    return (
+    if(!localStorage.getItem("username")) return <Login/>;
 
+    return (
         <ChatEngine
 			height="100vh"
-			userName="Infinium"
-			userSecret="ThisIsNaruto"
-			projectID="d5b60537-b785-4e16-bf7d-4d366a5ad8a5"
+			userName={localStorage.getItem("username")}
+			userSecret={localStorage.getItem("password")}
+			projectID={projectId}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
-
-        // <ChatEngine 
-        //     height="100vh"
-        //     userName={username}
-		// 	userSecret={usersceret}
-		// 	projectID={projectId}
-        // />
     );
 };
 
