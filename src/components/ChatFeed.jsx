c3b060e08dcf841787c16afd3a56087c1aee076a
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-09-04 17:01:36 +0530

    Chat complete

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -9,6 +9,22 @@ function ChatFeed(props){
 
     const chat = chats && chats[activeChat];
 
+    function renderReadReceipts(message, isMyMessage){
+        return (
+            chat.people.map((person, index) => person.last_read === message.id && 
+            (
+                <div
+                  key={`read_${index}`}
+                  className="read-receipt"
+                  style={{
+                    float: isMyMessage ? "right" : "left",
+                    backgroundImage: `url(${person?.person?.avatar})`
+                  }}
+                />
+            )
+        ));
+    }
+
     function renderMessages(){
         const keys = Object.keys(messages);
 
@@ -27,7 +43,7 @@ function ChatFeed(props){
                     </div> 
 
                     <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
-                        read-receipts
+                        {renderReadReceipts(message, isMyMessage)}
                     </div>
 
                 </div>
